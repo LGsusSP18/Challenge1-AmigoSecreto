fc6e974bd5f07a59cc0bad24599806c03623bfb9
LGsusSP18 <jsoriapineda@gmail.com>
2025-08-12 12:24:36 -0600

    Vinculamos app.js con index.html y comenzamos desarrollo de funciones

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let friends=[];
+
+function addFriend(){
+    return;
+}
+
+function updateFriend(params){
+    return;
+}
+
+function sortFriend(params){
+    return;
+}
