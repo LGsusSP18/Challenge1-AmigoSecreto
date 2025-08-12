e33d8ebbe3dd18bdfeeeb5b81ed81ee3d5b5b10a
LGsusSP18 <jsoriapineda@gmail.com>
2025-08-12 13:43:01 -0600

    La función updateFriend() se completa y se agrega función auxiliar listFriends()

--- app.js
+++ app.js
@@ -4,14 +4,32 @@ let friends=[];
 
 function addFriend(){
     let NFriend=document.getElementById("friend").value;
-    friends.push(NFriend);
-    console.log(friends);
-    updateFriend();
+    if(NFriend==""){
+        alert("Por favor ingrese un nombre")
+    }
+    else{
+        friends.push(NFriend);
+        console.log(friends);
+        updateFriend();
+        document.getElementById("friend").value="";
+    }
     return;
 }
 
 function updateFriend(){
+    let list=document.getElementById("listaAmigos");
+    list.innerHTML="";
+    list.innerHTML=listFriends();
+    console.log(listFriends());
     return;
+}
+
+function listFriends() {
+    var text="";
+    for (let i =0;i<friends.length;i++){
+        text=text+"<li>"+friends[i]+"</li>";
+    }
+    return text;
 }
 
 function sortFriend(){
